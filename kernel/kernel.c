ca6eede24ff2ebeb69974d07b3a61528763e2319
Einliterflasche <einliterflasche@pm.me>
2022-05-02 15:15:56 +0200

    Add: persist vga pointer in print function

--- kernel/kernel.c
+++ kernel/kernel.c
@@ -1,11 +1,25 @@
 void print(char* msg) {
-    int** cursor = (int**) 0xb7f0;
-    *cursor = 0xb8000;
-    int* current = *current;
-    *current = 'X';
+    // This the address in memory where the cursor is stored
+    const char** CURSOR_LOC = (const char**) 0xb7f0;
+
+    // Init cursor at top left corner if it has not been inited yet
+    if (*CURSOR_LOC <= (char*) 0xb8000) 
+        *CURSOR_LOC = (char*) 0xb8000;
+   
+    // Get current cursor
+    char* cursor = (char*) *CURSOR_LOC;
+
+    // Write the string with the current cursor
+    while (*msg != 0) {
+        *cursor++ = *msg++;
+        *cursor++ = 0x0f;
+    } 
+
+    // Store the new cursor in memory
+    *CURSOR_LOC = cursor;
 }
 
 void main() {
-    char* msg = "Hello World from kernel!";
+    char* msg = "Hello, kernel!";
     print(msg);
 }
